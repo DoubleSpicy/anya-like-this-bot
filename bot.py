a7ff6bb31f8b85951db326f1efbf62f5c19da51b
DoubleSpicy <stven3au@gmail.com>
2022-08-24 23:21:33 +0800

    implemented bot main body

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 # main body of bot
+from email import message
 from flask import Flask
 from flask import request
 from flask import Response
@@ -6,6 +7,10 @@ import requests
 
 import os, yaml # loading configs
 
+import telegram
+import logging
+
+from cv_script import produce_output
 
 def parse_config_path(filename: str):
     return os.path.abspath(os.path.dirname(__file__)) + "/config/" + filename
@@ -14,10 +19,16 @@ def parse_config_path(filename: str):
 config = yaml.safe_load(open(parse_config_path('bot_settings.yml')))
 TOKEN = config['TOKEN']
 
+bot = telegram.Bot(token=TOKEN)
 app = Flask(__name__)
+
+
+
+
 
 def tel_parse_message(message):
     print("message-->",message)
+    print(type(message))
     try:
         chat_id = message['message']['chat']['id']
         txt = message['message']['text']
@@ -28,6 +39,54 @@ def tel_parse_message(message):
     except:
         print("NO text found-->>")
  
+def msgHandler(message):
+    # msg['message']['reply_to_message']['photo'][-1]
+    chat_id=message['message']['chat']['id']
+    if message['message'].get('reply_to_message'):
+        if message['message']['reply_to_message'].get('document'):
+            if message['message']['reply_to_message']['document']['mime_type'] == 'image/jpeg' or message['message']['reply_to_message']['document']['mime_type'] == 'image/png':
+                file_id = message['message']['reply_to_message']['document']['thumb']['file_id']
+                url = f"https://api.telegram.org/bot{TOKEN}/getFile?file_id={file_id}"
+                download_and_process(url, chat_id=chat_id)
+        elif message['message']['reply_to_message'].get('photo'):
+            file_id = message['message']['reply_to_message']['photo'][-1]['file_id']
+            print("file_id-->", file_id)
+            # file_id = file_info['file_id']
+            url = f"https://api.telegram.org/bot{TOKEN}/getFile?file_id={file_id}"
+            download_and_process(url, chat_id=chat_id)
+
+def download_and_process(url, chat_id):
+    r = requests.get(url)
+    if r.status_code == 200:
+        # return ok, use file_id and chat id to download the image.
+        file_id = r.json()['result']['file_id']
+        file_path = r.json()['result']['file_path']
+        print(r.json())
+        print(file_id, file_path)
+        # Download photo
+        file_name = os.path.basename(file_path)
+        response = requests.get('https://api.telegram.org/file/bot%s/%s' % (TOKEN, file_path))
+        dst_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)) + '/temp', 'target.jpg')
+        print(dst_file_path)
+        with open(dst_file_path, 'wb') as f:
+            f.write(response.content)
+        print(u"Downloaded file to {}".format(dst_file_path))
+
+        # call openCV to make new output
+        produce_output(dst_file_path)
+        
+        # upload this photo
+        send_photo(chat_id=chat_id, image_path='./temp/output.jpg')
+        os.remove(dst_file_path)
+        os.remove('./temp/output.jpg')
+
+def send_photo(chat_id, image_path, image_caption="WAKU WAKU!"):
+    data = {"chat_id": chat_id, "caption": image_caption}
+    url = f'https://api.telegram.org/bot{TOKEN}/sendPhoto?chat_id={chat_id}'
+    with open(image_path, "rb") as image_file:
+        ret = requests.post(url, data=data, files={"photo": image_file})
+    return ret.json()
+
 def tel_send_message(chat_id, text):
     url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
     payload = {
@@ -49,23 +108,47 @@ def tel_send_image(chat_id, caption: str):
  
     r = requests.post(url, json=payload)
     return r
- 
+
+
+def get_json(method_name, *args, **kwargs):
+    return make_request('get', method_name, *args, **kwargs)
+
+def post_json(method_name, *args, **kwargs):
+    return make_request('post', method_name, *args, **kwargs)
+
+def make_request(method, method_name, *args, **kwargs):
+    response = getattr(requests, method)(
+        'https://api.telegram.org/bot%s/%s' % (TOKEN, method_name),
+        *args, **kwargs
+    )
+    if response.status_code > 200:
+        raise DownloadError(response)
+    return response.json()
+
+class DownloadError(Exception):
+    pass
+
+
+
+
+
 @ app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         msg = request.get_json()
-        try:
-            chat_id, txt = tel_parse_message(msg)
-            if txt == "like this":
-                # tel_send_message(chat_id,"Hello, world!")
-                tel_send_image(chat_id, 'WAKU WAKU!')
-        #     elif txt == "image":
-        #         tel_send_image(chat_id)
- 
-        #     else:
-        #         tel_send_message(chat_id, 'from webhook')
-        except:
-            print("fail to parse and send response!")
+        # try:
+        chat_id, txt = tel_parse_message(msg)
+        # print(msg)
+        print('=================')
+        print(msg['message']['reply_to_message']['photo'][-1])
+        print(txt)
+        print('=================')
+        if txt == "like this":
+            print('triggered!!')
+            msgHandler(msg)
+        # except Exception as e:
+        #     print("fail to parse and send response!")
+        #     print(e)
  
         return Response('ok', status=200)
     else:
--- cv_script.py
+++ cv_script.py
@@ -2,7 +2,6 @@ import cv2 as cv
 import numpy as np
 # from matplotlib import pyplot as plt
 
-tv_size = (600, 780)
 
 def image_resize(image, width = None, height = None, inter = cv.INTER_AREA):
     # initialize the dimensions of the image to be resized and
@@ -38,48 +37,47 @@ def image_resize(image, width = None, height = None, inter = cv.INTER_AREA):
 
 # def resize_v2():
 
-target = cv.imread(r'./resources/typhoon.jpg', 1)
-target = image_resize(target, height=500, width=650)
-# target = cv.resize(target, (650, 650))
-target2 = np.zeros((1080, 1728, 3), np.uint8)
-print(target2.shape)
-print(target.shape)
-offset_y = round((target2.shape[0]/2 - target.shape[0]/2))
-offset_x = round((target2.shape[1]/2 - target.shape[1]/2)) 
-print(offset_y, offset_x)
-target2[160:160+target.shape[0], 505: 505+target.shape[1]] = target
-cv.imshow('target', target2)
+def produce_output(target_name):  
+    target = cv.imread(r'./temp/target.jpg', 1)
+    target = image_resize(target, height=500, width=650)
+    # target = cv.resize(target, (650, 650))
+    target2 = np.zeros((1080, 1728, 3), np.uint8)
+    print(target2.shape)
+    print(target.shape)
+    offset_y = round((target2.shape[0]/2 - target.shape[0]/2))
+    offset_x = round((target2.shape[1]/2 - target.shape[1]/2)) 
+    print(offset_y, offset_x)
+    target2[160:160+target.shape[0], 505: 505+target.shape[1]] = target
+    # cv.imshow('target', target2)
 
 
-src = cv.imread(r'./resources/base.jpg')
-imgRGB = cv.cvtColor(src, cv.COLOR_BGR2RGB)
-imgHSV = cv.cvtColor(imgRGB, cv.COLOR_BGR2HSV)
+    src = cv.imread(r'./resources/base.jpg')
+    imgRGB = cv.cvtColor(src, cv.COLOR_BGR2RGB)
+    imgHSV = cv.cvtColor(imgRGB, cv.COLOR_BGR2HSV)
 
-lower = np.array([60, 240, 245])    #Lower values of HSV range; Green have Hue value equal 120, but in opencv Hue range is smaler [0-180]
-upper = np.array([70, 255, 255])  #Uppervalues of HSV range
-imgRange = cv.inRange(imgHSV, lower, upper)
+    lower = np.array([60, 240, 245])    #Lower values of HSV range; Green have Hue value equal 120, but in opencv Hue range is smaler [0-180]
+    upper = np.array([70, 255, 255])  #Uppervalues of HSV range
+    imgRange = cv.inRange(imgHSV, lower, upper)
 
-# kernels for morphology operations
-kernel_noise = np.ones((3,3),np.uint8) #to delete small noises
-kernel_dilate = np.ones((23,23),np.uint8)  #bigger kernel to fill holes after ropes
-kernel_erode = np.ones((14,14),np.uint8)  #bigger kernel to delete pixels on edge that was add after dilate function
+    # kernels for morphology operations
+    kernel_noise = np.ones((3,3),np.uint8) #to delete small noises
+    kernel_dilate = np.ones((9,9),np.uint8)  #bigger kernel to fill holes after ropes
+    kernel_erode = np.ones((1,1),np.uint8)  #bigger kernel to delete pixels on edge that was add after dilate function
 
-imgErode = cv.erode(imgRange, kernel_noise, 1)
-imgDilate = cv.dilate(imgErode , kernel_dilate, 1)
-imgErode = cv.erode(imgDilate, kernel_erode, 1)
-
-
-mask = cv.cvtColor(imgErode, cv.COLOR_GRAY2BGR)
-res = cv.subtract(src, mask)
-# cv.imshow('res', res)
-
-# mask of target
-res_t = cv.bitwise_and(target2, target2, mask = imgErode)
-
-res_f = cv.add(res, res_t)
-cv.imshow('final', res_f)
-cv.imwrite('anya-likes-typhoon8.jpg', res_f)
-# cv.imshow('res_t', res_t)
+    imgErode = cv.erode(imgRange, kernel_noise, 1)
+    imgDilate = cv.dilate(imgErode , kernel_dilate, 1)
+    imgErode = cv.erode(imgDilate, kernel_erode, 1)
 
 
-cv.waitKey(0)
+    mask = cv.cvtColor(imgErode, cv.COLOR_GRAY2BGR)
+    res = cv.subtract(src, mask)
+    # # cv.imshow('res', res)
+
+    # mask of target
+    res_t = cv.bitwise_and(target2, target2, mask = imgErode)
+
+    res_f = cv.add(res, res_t)
+    # cv.imshow('final', res_f)
+    cv.imwrite('./temp/output.jpg', res_f)
+    # # cv.imshow('res_t', res_t)
+
